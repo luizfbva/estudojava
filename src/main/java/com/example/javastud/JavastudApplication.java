cb939a6bb6ee254a21866056c93655d9cee6c512
Luiz Fernando <luizfbva@gmail.com>
2024-07-08 13:03:31 -0300

    Project created

---
+++ src/main/java/com/example/javastud/JavastudApplication.java
@@ -0,0 +1,13 @@
+package com.example.javastud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavastudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javastud/JavastudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javastud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavastudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
